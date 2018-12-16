fd6154a8142d11c47913efac9c77ca9bef6829b9
xtf <107188228@qq.com>
2018-12-16 21:37:26 +0800

    00002--modify login 测试

--- src/com/dou/LoginTest.java
+++ src/com/dou/LoginTest.java
@@ -3,6 +3,8 @@ package com.dou;
 public class LoginTest {
 
 	public static void main(String[] args) {
+		System.out.println("ºÇºÇºÇºÇºÇ");
 		System.out.println("¹þ¹þ¹þ¹þ¹þ");
+		System.out.println("À²À²À²À²À²");
 	}
 }
